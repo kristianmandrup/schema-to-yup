95057658cb4fecf28424698162552f27d7a4ce00
Kristian Mandrup <kmandrup@gmail.com>
2019-11-07 09:20:31 +0000

    improve constraints mapper

--- src/adapters/base/constraint-mapper.js
+++ src/adapters/base/constraint-mapper.js
@@ -1,14 +1,32 @@
 import camelcase from "camelcase";
 
-const constraintsFor = constraintClassesMap => {
-  const constraintClassNames = Object.keys(constraintClassesMap);
+const hasApplyMethod = (clazz = clazz.prototype && clazz.prototype.apply);
 
+const error = (clazz, opts) => {
+  const { key } = opts;
+  const msg = `constraint class for ${key} invalid: missing apply method`;
+  console.error(msg, { clazz, opts });
+  throw msg;
+};
+
+const defaults = {
+  constraintFactoryFn: (typeHandler, opts = {}) => {
+    const { clazz, key } = opts;
+    hasApplyMethod(clazz, opts) ? new clazz(typeHandler).apply() : error(clazz);
+  }
+};
+
+export const constraintsFor = (constraintClassesMap, opts = {}) => {
+  const constraintClassNames = Object.keys(constraintClassesMap);
+  const constraintFactoryFn =
+    defaults.constraintFactoryFn || opts.constraintFactoryFn;
   // export const createNoUnknown =
 
   return constraintClassNames.reduce((acc, name) => {
     const key = camelcase(name);
     const clazz = constraintClasses[name];
-    acc[key] = typeHandler => new clazz(typeHandler).apply();
+    acc[key] = typeHandler =>
+      constraintFactoryFn(typeHandler, { clazz, name, key });
     return acc;
   }, {});
 };
