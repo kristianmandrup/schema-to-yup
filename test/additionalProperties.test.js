e97decedd110ca92818f853905d10c9f4189b9d4
Kristian Mandrup <kmandrup@gmail.com>
2019-12-03 13:56:07 +0000

    additionalProperties test fails

---
+++ test/additionalProperties.test.js
@@ -0,0 +1,33 @@
+// additionalProperties: https://github.com/kristianmandrup/schema-to-yup/issues/55
+
+const { buildYup } = require("../src");
+
+describe("models schema", () => {
+  const modelsJsonSchema = {
+    title: "models",
+    type: "object",
+    properties: {
+      name: {
+        type: "string",
+        required: true
+      },
+      type: {
+        type: "string",
+        required: true
+      }
+    },
+    additionalProperties: { type: "string" }
+  };
+
+  const schema = buildYup(modelsJsonSchema);
+
+  test("invalid json is invalid", async () => {
+    const valid = schema.isValidSync({
+      type: "Person",
+      name: "Administrator",
+      level: 3, // invalid,
+      color: "blue" // valid
+    });
+    expect(valid).toBe(false);
+  });
+});
